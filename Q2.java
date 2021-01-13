5786378d4b2dd2dbf9fcf16f0b5a91ff24263e23
Sidharth Roy <73808133+sidharth582@users.noreply.github.com>
2021-01-13 15:10:28 +0530

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -3,14 +3,17 @@ import java.util.regex.*;
 import java.util.*;
 import java.util.StringTokenizer;
 public class Q2 {
+	static void replace(String s1,String s2) {
+		String regex="["+s2+"]";
+		s1=s1.replaceAll(regex,"");
+		System.out.println(s1);
+	}
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("First String");
 		String fs=sc.nextLine();
 		System.out.println("second String");
 		String ss=sc.nextLine();
-		String regex="["+ss+"]";
-		fs=fs.replaceAll(regex,"");
-		System.out.println(fs);
+		replace(fs,ss);	
 	}
 }
